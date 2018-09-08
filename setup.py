637928bebf5fe912f05c7ea215a8d21b656e6c17
kezian <kezian@umich.edu>
2018-09-08 18:41:01 -0400

    Finish setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='github-text-crawler',
+    version='0.1.0',
+    author='Zian Ke',
+    author_email='kezian@umich.edu',
+    url='https://github.com/zianke/github-text-crawler',
+    description='Extract documentation and commit logs from GitHub repository.',
+    license='MIT',
+    packages=['github-text-crawler'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['certifi==2018.8.24',
+                      'chardet==3.0.4',
+                      'click==6.7',
+                      'idna==2.7',
+                      'requests==2.19.1',
+                      'urllib3==1.23'],
+)
